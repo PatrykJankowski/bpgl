326fe8c7f3f1001a9becf7d958bc4252e5142052
PatrykJankowski <patryk.k.jankowski@gmail.com>
2016-12-10 21:21:43 +0100

    ...

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -155,7 +155,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media_cdn/')
+MEDIA_ROOT = '/home/ubuntu/biblioteka/media/'
 
 SUMMERNOTE_CONFIG = {
 

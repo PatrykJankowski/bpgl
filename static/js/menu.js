9c3c760ef94162c5b4d0bd9505194f56566e9ac3
PatrykJankowski <patryk.k.jankowski@gmail.com>
2016-12-18 18:16:23 +0100

    ...

---
+++ static/js/menu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("a[href='#katalogi").prop('href', '/#katalogi')
+    $("a[href='#aktualnosci").prop('href', '/wpisy/?kategoria=aktualnosci')
+    $("a[href='#ksiazki").prop('href', '/wpisy/?kategoria=ksiazki')
+    $("a[href='#o_bibliotece").prop('href', '/o-bibliotece')
+    $("a[href='#galeria").prop('href', '/galeria')
+});

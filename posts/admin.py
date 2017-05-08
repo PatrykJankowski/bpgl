7bab53edd38d2806308f3440f72b72a4557e0310
PatrykJankowski <patryk.k.jankowski@gmail.com>
2017-05-08 13:02:05 +0200

    admin slider

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Slider
 from django_summernote.admin import SummernoteModelAdmin
 
 

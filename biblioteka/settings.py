cd7ec6b64bc3feb012d0626be873b1f5d8412dbe
PatrykJankowski <patryk.jankowski@protonmail.com>
2018-08-29 22:25:44 +0200

    cookies

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&mt!w_-38o$n07*^w=l=j00f44dxje32c*ie#*e9-ijm9or9ct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['bpgl.pl', 'www.bpgl.pl', 'ec2-54-93-54-71.eu-central-1.compute.amazonaws.com', '127.0.0.1', 'localhost', '54.93.54.71', 'strzepcz.ssd-linuxpl.com']

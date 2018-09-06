680395b7c7f6b20c6f6fa4867fa03272727c8ec6
PatrykJankowski <patryk@arthro.ai>
2018-09-06 11:05:50 +0200

    debug

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&mt!w_-38o$n07*^w=l=j00f44dxje32c*ie#*e9-ijm9or9ct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #TEMPLATE_DEBUG = DEBUG
 
 SECURE_SSL_REDIRECT = True

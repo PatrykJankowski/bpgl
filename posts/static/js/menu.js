a187100014421a4947e38fe1dc3de65f6d1e52dc
PatrykJankowski <patryk.jankowski@protonmail.com>
2021-01-27 20:28:11 +0100

    wcga and mobile version updates

--- posts/static/js/menu.js
+++ posts/static/js/menu.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
     $("a[href='#aktualnosci").prop('href', '/wpisy/?kategoria=aktualnosci')
     $("a[href='#ksiazki").prop('href', '/wpisy/?kategoria=ksiazki')
     $("a[href='#o_bibliotece").prop('href', '/o-bibliotece')
-    $("a[href='#galeria").prop('href', '/galeria')
+    //$("a[href='#galeria").prop('href', '/galeria')
+    $("a[href='#kontakt").prop('href', '/kontakt')
 });
